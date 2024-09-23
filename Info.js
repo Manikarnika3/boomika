fc5761a8bcb580f7970c5fbaf639cdbe007b525f
unknown <rboomika804@gmail.com>
2024-09-24 00:31:53 +0530

    first commit

---
+++ Info.js
@@ -0,0 +1,22 @@
+function info { <
+    div class = "main" >
+        <
+        label
+    for = "" > name: < /label>  <
+        Input type = "text" > < /Input>  <
+        label
+    for = "" > email: < /label>  <
+        Input type = "text" > < /Input>  <
+        label
+    for = "" > dob: < /label>  <
+        Input type = "num" > < /Input>  <
+        label
+    for = "" > age: < /label> <
+    Input type = "num" > < /Input>  <
+        label
+    for = "" > gender: < /label>  <
+        Input type = "text" > < /Input>
+
+    <
+    /div>
+}
